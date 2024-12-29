acfed01817c021f1e807fdcc8839b69415e77ca5
whyyod <Sriintayod@gmail.com>
2024-12-30 01:03:22 +0700

    Complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 #include <cmath>
 using namespace std;
 
@@ -10,12 +9,37 @@ int main(){
     int N = sizeof(A)/sizeof(A[0]);
     double B[6];
     stat(A,N,B);
-    cout << fixed << setprecision(2);
-    cout << "Arithmetic Mean = " << B[0] << endl;
-    cout << "Standard Deviation = " << B[1] << endl;
-    cout << "Geometric Mean = " << B[2] << endl;
-    cout << "Harmonic Mean = " << B[3] << endl;
-    cout << "Max = " << B[4] << endl;
-    cout << "Min = " << B[5];
+    cout << "Arithmetic Mean = " << B[0];
+    cout << "\nStandard Deviation = " << B[1];
+    cout << "\nGeometric Mean = " << B[2];
+    cout << "\nHarmonic Mean = " << B[3];
+    cout << "\nMax = " << B[4];
+    cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[],int N,double B[]) {
+double sum = 0 ,  SD = 0 , GM = 1 , HM = 0, max = A[0] , min = A[0];
+
+
+for (int i = 0 ; i < N ; ++i){
+    sum += A[i] ;
+    GM *= A[i] ;
+    SD += pow(A[i],2) ;
+    HM += 1/A[i] ;
+}
+
+
+for (int i = 0 ; i < N ; ++i){
+if (A[i] > max) max = A[i];
+if (A[i] < min) min = A[i];
+}
+
+B[0] = sum/N ;
+B[1] = sqrt(SD/N - pow(sum/N,2));
+B[2] = pow(GM,1.0/N); 
+B[3] = N / HM;
+B[4] = max;
+B[5] = min;
+
 }
